d2a010b72f2c4a992315ab8eff712494cd4140f3
lekang <lekang@houzz.com>
2016-08-22 14:05:50 -0700

    move output in a different disk and generate a symlink for output

--- tools/gen_data_links.sh
+++ tools/gen_data_links.sh
@@ -2,6 +2,7 @@
 
 #execute this script from the root of this project: py-faster-rcnn
 
-ln -s /drive3/work_data ./work_data
-ln -s /drive3/data/HouzzDataCollection ./data/HouzzDataCollection
-# ln -s /root/packages/py-faster-rcnn/output ./output
+ln -s /drive3/work_data ./
+ln -s /drive3/data/HouzzDataCollection ./data/
+ln -s /drive3/data/resnet_models ./data/
+ln -s /drive3/output ./
